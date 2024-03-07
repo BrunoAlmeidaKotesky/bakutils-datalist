272b914a46fb4c1deeb6aa0f9bd812b4df2314c7
BrunoAlmeidaKotesky <blasterplayersbr@gmail.com>
2024-03-07 19:45:18 -0300

    1.1.1

--- lib/index.ts
+++ lib/index.ts
@@ -5,3 +5,4 @@ export * from './models/DataListStore';
 export * from './models/ColumnItemTransformation';
 export * from './plugins';
 export * from './helpers/objectUtilities';
+export * from 'bakutils-types';
